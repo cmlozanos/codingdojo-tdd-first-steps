7e75ff8966795eb204001a9a1bd1686a007bdb12
Carlos Lozano <carlosmlozanos@gmail.com>
2020-07-16 18:15:46 +0200

    set first char allways A

---
+++ src/main/java/cmls/spring/codingdojotddfirststeps/Diamond.java
@@ -0,0 +1,17 @@
+package cmls.spring.codingdojotddfirststeps;
+
+/**
+ * Definicion del objeto diamante
+ * @author Carlos Lozano
+ *
+ */
+public class Diamond {
+	private String firstChar;
+	private String lastChar;
+
+	public Diamond(String lastChar) {
+		super();
+		this.firstChar = "A";
+		this.lastChar = lastChar;
+	}
+}
