591dc67770fff078ea546975ae3be11009473b58
Carlos Lozano <carlosmlozanos@gmail.com>
2020-07-16 14:23:06 +0200

    first commit

---
+++ src/main/java/cmls/spring/codingdojotddfirststeps/CodingdojoTddFirstStepsApplication.java
@@ -0,0 +1,13 @@
+package cmls.spring.codingdojotddfirststeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingdojoTddFirstStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingdojoTddFirstStepsApplication.class, args);
+	}
+
+}
